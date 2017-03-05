8dcb590f37d6debb4ec3e6c5ce9cd13c3206b5d7
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-03-05 14:42:03 +0200

    add graph logic

--- app/js/controllers/graph.controller.js
+++ app/js/controllers/graph.controller.js
@@ -2,6 +2,88 @@
  * Created by Iaroslav Zhbankov on 05.03.2017.
  */
 app.controller('GraphCtrl', ['$scope', 'Data', function ($scope, Data) {
+
+    var ctx = document.getElementById("myChart").getContext("2d");
+
+    var data = {
+        labels: ["Скорость получения услуги",
+            "Доверие",
+            "Привлекательность тарифов",
+            "Цена – качество",
+            "Объем страхового покрытия",
+            "Размер франшизы",
+            "Количество услуг",
+            "Степень учета рисков",
+            "Простота норм договора",
+            "Быстрота, качество и справедливость"],
+        datasets: [
+            {
+                label: "Конкурент",
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)'
+                ],
+                borderWidth: 1,
+                data: Data.getCompetitorsData()
+            }, {
+                label: "Компания",
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1,
+                data: Data.getCustomersData()
+            }
+        ]
+    };
+
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {}
+    });
+
+
     $scope.customers = {
         customer: 1,
         availableOptions: [
@@ -13,6 +95,6 @@ app.controller('GraphCtrl', ['$scope', 'Data', function ($scope, Data) {
             {id: 'customer-6', name: '6'}
         ]
     };
-    console.log(Data.getCustomersData());
-    console.log(Data.getCompetitorsData());
+    //console.log(Data.getCustomersData());
+    //console.log(Data.getCompetitorsData());
 }]);
