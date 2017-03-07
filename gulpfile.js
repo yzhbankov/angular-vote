08d6d0b3964a3d603dd9b3dab7551bbb25c5f811
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-03-07 22:37:56 +0200

    prepeare for heroku deploy

--- gulpfile.js
+++ gulpfile.js
@@ -30,17 +30,17 @@ gulp.task('server-start', function(){
 
 //style
 gulp.task('style', function(){
-    return gulp.src('app/sass/**/*.sass')
+    return gulp.src('src/sass/**/*.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix({
             browsers: ['last 15 versions']
         }))
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('src/css'));
 });
 
 //build production files
 gulp.task('build', function(){
-    return gulp.src('app/*.html')
+    return gulp.src('src/*.html')
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
@@ -49,7 +49,7 @@ gulp.task('build', function(){
 
 //watch
 gulp.task('watch', function(){
-    gulp.watch('app/sass/**/*.sass', ['style'])
+    gulp.watch('src/sass/**/*.sass', ['style'])
 });
 
 gulp.task('default', ['start', 'server-start', 'watch']);
