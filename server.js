23ad99ca96e0d79ec40470d87b1d6916c8622cc5
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-03-08 14:17:07 +0200

    prepeare to deploying to heroku routing problem

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(function (req, res, next) {
 });
 app.use(express.static(__dirname + '/app'));
 
+app.set('views', __dirname + '/app');
+app.set('view engine', 'html');
+
 app.get('/', function (req, res) {
     res.render('index');
 });
