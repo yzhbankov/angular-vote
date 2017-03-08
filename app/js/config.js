6e0a208eda76ce43ac0d9cde3fb7d8192123fc1e
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-03-08 13:48:49 +0200

    prepeare to deploying to heroku routing problem

--- app/js/config.js
+++ app/js/config.js
@@ -16,7 +16,7 @@ app.config(function ($routeProvider, $locationProvider) {
         .when('/problem', {
             templateUrl: 'js/views/problemView.html'
         })
-        .when('/user-signup', {
+        .when('/signup', {
             templateUrl: 'js/views/signupview.html'
         })
         .when('/signin', {
